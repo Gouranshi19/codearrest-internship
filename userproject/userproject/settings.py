f63c35b0109655869bcfa58511d987082b978f9c
Gouranshi19 <gouranshi19@gmail.com>
2026-06-10 14:09:10 +0530

    add gitignore

--- userproject/userproject/settings.py
+++ userproject/userproject/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_URL = '/login'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+LOGIN_URL = '/login'
